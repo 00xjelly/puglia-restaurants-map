b29f3c330c5f713c4dbbca2fe9742883e6c5fdf8
00xjelly <angelomtgarcia@gmail.com>
2025-04-03 10:37:52 +0200

    Add complete restaurant data

---
+++ restaurants.js
@@ -0,0 +1,638 @@
+// Restaurant data for Puglia, Italy
+const restaurants = [
+    // Alberobello
+    {
+        name: "Coppino / \"Come Nonna Comanda\"",
+        city: "Alberobello",
+        coords: [40.7861, 17.2406],
+        address: "Alberobello, Puglia, Italy"
+    },
+    {
+        name: "Miseria e Nobiltà",
+        city: "Alberobello",
+        coords: [40.7861, 17.2406],
+        address: "Alberobello, Puglia, Italy"
+    },
+    {
+        name: "Trullo D'Oro Restaurant",
+        city: "Alberobello",
+        coords: [40.7861, 17.2406],
+        address: "Alberobello, Puglia, Italy"
+    },
+    
+    // Altamura
+    {
+        name: "Dicecca World",
+        city: "Altamura",
+        coords: [40.8265, 16.5524],
+        address: "Altamura, Puglia, Italy"
+    },
+    
+    // Andria
+    {
+        name: "Ristorante Antichi Sapori",
+        city: "Andria",
+        coords: [41.2197, 16.2926],
+        address: "Andria, Puglia, Italy"
+    },
+    
+    // Bari
+    {
+        name: "Ai 2 Ghiottoni",
+        city: "Bari",
+        coords: [41.1171, 16.8719],
+        address: "Bari, Puglia, Italy"
+    },
+    {
+        name: "Al Sorso Preferito",
+        city: "Bari",
+        coords: [41.1171, 16.8719],
+        address: "Bari, Puglia, Italy"
+    },
+    {
+        name: "El Chiringuito",
+        city: "Bari",
+        coords: [41.1171, 16.8719],
+        address: "Bari, Puglia, Italy"
+    },
+    {
+        name: "Mastrociccio",
+        city: "Bari",
+        coords: [41.1171, 16.8719],
+        address: "Bari, Puglia, Italy"
+    },
+    {
+        name: "La Uascezze",
+        city: "Bari",
+        coords: [41.1171, 16.8719],
+        address: "Bari, Puglia, Italy"
+    },
+    {
+        name: "El Pedro",
+        city: "Bari",
+        coords: [41.1171, 16.8719],
+        address: "Bari, Puglia, Italy"
+    },
+    {
+        name: "Il Pane e le Rose",
+        city: "Bari",
+        coords: [41.1171, 16.8719],
+        address: "Bari, Puglia, Italy"
+    },
+    {
+        name: "La Locanda dell'Elfo",
+        city: "Bari",
+        coords: [41.1171, 16.8719],
+        address: "Bari, Puglia, Italy"
+    },
+    {
+        name: "Osteria Le Arpie",
+        city: "Bari",
+        coords: [41.1171, 16.8719],
+        address: "Bari, Puglia, Italy"
+    },
+    {
+        name: "Panificio Fiore",
+        city: "Bari",
+        coords: [41.1171, 16.8719],
+        address: "Bari, Puglia, Italy"
+    },
+    {
+        name: "Panificio Santa Rita",
+        city: "Bari",
+        coords: [41.1171, 16.8719],
+        address: "Bari, Puglia, Italy"
+    },
+    {
+        name: "PerBacco",
+        city: "Bari",
+        coords: [41.1171, 16.8719],
+        address: "Bari, Puglia, Italy"
+    },
+    {
+        name: "Porto Vecchio Fish Market, Mola San Nicola",
+        city: "Bari",
+        coords: [41.1171, 16.8719],
+        address: "Bari, Puglia, Italy"
+    },
+    {
+        name: "Le Sgagliozze di Donna Carmela",
+        city: "Bari",
+        coords: [41.1171, 16.8719],
+        address: "Bari, Puglia, Italy"
+    },
+    {
+        name: "Tiella",
+        city: "Bari",
+        coords: [41.1171, 16.8719],
+        address: "Bari, Puglia, Italy"
+    },
+    {
+        name: "U'Kor",
+        city: "Bari",
+        coords: [41.1171, 16.8719],
+        address: "Bari, Puglia, Italy"
+    },
+    {
+        name: "Urban L'Assassineria Urbana",
+        city: "Bari",
+        coords: [41.1171, 16.8719],
+        address: "Bari, Puglia, Italy"
+    },
+    
+    // Brindisi
+    {
+        name: "La Locanda del Porto",
+        city: "Brindisi",
+        coords: [40.6428, 17.9429],
+        address: "Brindisi, Puglia, Italy"
+    },
+    
+    // Carovigno
+    {
+        name: "Braceria Pomodoro",
+        city: "Carovigno",
+        coords: [40.7049, 17.6581],
+        address: "Carovigno, Puglia, Italy"
+    },
+    {
+        name: "Dissapore Ristorante di Andrea Catalano",
+        city: "Carovigno",
+        coords: [40.7049, 17.6581],
+        address: "Carovigno, Puglia, Italy"
+    },
+    {
+        name: "Miramare da Michele",
+        city: "Carovigno",
+        coords: [40.7049, 17.6581],
+        address: "Carovigno, Puglia, Italy"
+    },
+    {
+        name: "Picci Bar",
+        city: "Carovigno",
+        coords: [40.7049, 17.6581],
+        address: "Carovigno, Puglia, Italy"
+    },
+    {
+        name: "Pizzeria La Piccola Napoli",
+        city: "Carovigno",
+        coords: [40.7049, 17.6581],
+        address: "Carovigno, Puglia, Italy"
+    },
+    {
+        name: "Sommarco",
+        city: "Carovigno",
+        coords: [40.7049, 17.6581],
+        address: "Carovigno, Puglia, Italy"
+    },
+    {
+        name: "Osteria Già Sotto L'Arco",
+        city: "Carovigno",
+        coords: [40.7049, 17.6581],
+        address: "Carovigno, Puglia, Italy"
+    },
+    {
+        name: "Trattoria La Cantina",
+        city: "Carovigno",
+        coords: [40.7049, 17.6581],
+        address: "Carovigno, Puglia, Italy"
+    },
+    
+    // Castrignano del Capo
+    {
+        name: "Retrò – Casa Museo Bar, Pasticceria",
+        city: "Castrignano del Capo",
+        coords: [39.8323, 18.3496],
+        address: "Castrignano del Capo, Puglia, Italy"
+    },
+    
+    // Cisternino
+    {
+        name: "Al Vecchio Fornello",
+        city: "Cisternino",
+        coords: [40.7423, 17.4248],
+        address: "Cisternino, Puglia, Italy"
+    },
+    {
+        name: "Giardini 36",
+        city: "Cisternino",
+        coords: [40.7423, 17.4248],
+        address: "Cisternino, Puglia, Italy"
+    },
+    {
+        name: "Trattoria Bère Vecchie",
+        city: "Cisternino",
+        coords: [40.7423, 17.4248],
+        address: "Cisternino, Puglia, Italy"
+    },
+    {
+        name: "Zio Pietro",
+        city: "Cisternino",
+        coords: [40.7423, 17.4248],
+        address: "Cisternino, Puglia, Italy"
+    },
+    
+    // Ceglie Messapica
+    {
+        name: "Calce",
+        city: "Ceglie Messapica",
+        coords: [40.6464, 17.5177],
+        address: "Ceglie Messapica, Puglia, Italy"
+    },
+    {
+        name: "Da Gino",
+        city: "Ceglie Messapica",
+        coords: [40.6464, 17.5177],
+        address: "Ceglie Messapica, Puglia, Italy"
+    },
+    {
+        name: "EnoCocus Acini & Carbone",
+        city: "Ceglie Messapica",
+        coords: [40.6464, 17.5177],
+        address: "Ceglie Messapica, Puglia, Italy"
+    },
+    {
+        name: "Osteria l'Antico Arco",
+        city: "Ceglie Messapica",
+        coords: [40.6464, 17.5177],
+        address: "Ceglie Messapica, Puglia, Italy"
+    },
+    {
+        name: "Osteria del Capitolo",
+        city: "Ceglie Messapica",
+        coords: [40.6464, 17.5177],
+        address: "Ceglie Messapica, Puglia, Italy"
+    },
+    {
+        name: "L'Osteria del DivinGustare",
+        city: "Ceglie Messapica",
+        coords: [40.6464, 17.5177],
+        address: "Ceglie Messapica, Puglia, Italy"
+    },
+    
+    // Fasano
+    {
+        name: "La Locanda di Martumè",
+        city: "Fasano",
+        coords: [40.8353, 17.5890],
+        address: "Fasano, Puglia, Italy"
+    },
+    
+    // Gallipoli
+    {
+        name: "Ristorante Il Bastione",
+        city: "Gallipoli",
+        coords: [40.0559, 17.9922],
+        address: "Gallipoli, Puglia, Italy"
+    },
+    {
+        name: "Trattoria La Puritate",
+        city: "Gallipoli",
+        coords: [40.0559, 17.9922],
+        address: "Gallipoli, Puglia, Italy"
+    },
+    
+    // Gravina in Puglia
+    {
+        name: "Osteria La Murgiana",
+        city: "Gravina in Puglia",
+        coords: [40.8198, 16.4158],
+        address: "Gravina in Puglia, Puglia, Italy"
+    },
+    {
+        name: "Trattoria Mamma Mia",
+        city: "Gravina in Puglia",
+        coords: [40.8198, 16.4158],
+        address: "Gravina in Puglia, Puglia, Italy"
+    },
+    
+    // Grottaglie
+    {
+        name: "Macchiaviva Bistrot",
+        city: "Grottaglie",
+        coords: [40.5365, 17.4342],
+        address: "Grottaglie, Puglia, Italy"
+    },
+    {
+        name: "Trattoria la Luna nel Pozzo",
+        city: "Grottaglie",
+        coords: [40.5365, 17.4342],
+        address: "Grottaglie, Puglia, Italy"
+    },
+    
+    // Latiano
+    {
+        name: "Luppolo & Farina",
+        city: "Latiano",
+        coords: [40.5389, 17.7167],
+        address: "Latiano, Puglia, Italy"
+    },
+    
+    // Lecce
+    {
+        name: "3 Rane – Ristoro",
+        city: "Lecce",
+        coords: [40.3516, 18.1718],
+        address: "Lecce, Puglia, Italy"
+    },
+    {
+        name: "400 Gradi",
+        city: "Lecce",
+        coords: [40.3516, 18.1718],
+        address: "Lecce, Puglia, Italy"
+    },
+    {
+        name: "La Cucina di Mamma Elvira",
+        city: "Lecce",
+        coords: [40.3516, 18.1718],
+        address: "Lecce, Puglia, Italy"
+    },
+    {
+        name: "Primo",
+        city: "Lecce",
+        coords: [40.3516, 18.1718],
+        address: "Lecce, Puglia, Italy"
+    },
+    {
+        name: "Vico del Cuciniere",
+        city: "Lecce",
+        coords: [40.3516, 18.1718],
+        address: "Lecce, Puglia, Italy"
+    },
+    {
+        name: "Le Zie Trattoria Casereccia",
+        city: "Lecce",
+        coords: [40.3516, 18.1718],
+        address: "Lecce, Puglia, Italy"
+    },
+    {
+        name: "Bros'",
+        city: "Lecce",
+        coords: [40.3516, 18.1718],
+        address: "Lecce, Puglia, Italy"
+    },
+    
+    // Martina Franca
+    {
+        name: "Pane Amore e Fantasia",
+        city: "Martina Franca",
+        coords: [40.7012, 17.3323],
+        address: "Martina Franca, Puglia, Italy"
+    },
+    {
+        name: "Ristorante Dal brigante By Enrico",
+        city: "Martina Franca",
+        coords: [40.7012, 17.3323],
+        address: "Martina Franca, Puglia, Italy"
+    },
+    
+    // Monopoli
+    {
+        name: "La Torretta del Pescatore",
+        city: "Monopoli",
+        coords: [40.9496, 17.3033],
+        address: "Monopoli, Puglia, Italy"
+    },
+    
+    // Ostuni
+    {
+        name: "Al Solito Posto",
+        city: "Ostuni",
+        coords: [40.7318, 17.5768],
+        address: "Ostuni, Puglia, Italy"
+    },
+    {
+        name: "Casa San Giacomo",
+        city: "Ostuni",
+        coords: [40.7318, 17.5768],
+        address: "Ostuni, Puglia, Italy"
+    },
+    {
+        name: "Trattoria Il Cortiletto",
+        city: "Ostuni",
+        coords: [40.7318, 17.5768],
+        address: "Ostuni, Puglia, Italy"
+    },
+    {
+        name: "Ristorante Trattoria Fave e Fogghje",
+        city: "Ostuni",
+        coords: [40.7318, 17.5768],
+        address: "Ostuni, Puglia, Italy"
+    },
+    {
+        name: "Gabó",
+        city: "Ostuni",
+        coords: [40.7318, 17.5768],
+        address: "Ostuni, Puglia, Italy"
+    },
+    {
+        name: "Masseria Moroseta",
+        city: "Ostuni",
+        coords: [40.7318, 17.5768],
+        address: "Ostuni, Puglia, Italy"
+    },
+    {
+        name: "Osteria Monacelle",
+        city: "Ostuni",
+        coords: [40.7318, 17.5768],
+        address: "Ostuni, Puglia, Italy"
+    },
+    {
+        name: "Ostuni Bistrot",
+        city: "Ostuni",
+        coords: [40.7318, 17.5768],
+        address: "Ostuni, Puglia, Italy"
+    },
+    {
+        name: "La Pastasciutta",
+        city: "Ostuni",
+        coords: [40.7318, 17.5768],
+        address: "Ostuni, Puglia, Italy"
+    },
+    
+    // Otranto
+    {
+        name: "LaltroBaffo",
+        city: "Otranto",
+        coords: [40.1462, 18.4912],
+        address: "Otranto, Puglia, Italy"
+    },
+    
+    // Peschici
+    {
+        name: "Al Trabucco da Mimì",
+        city: "Peschici",
+        coords: [41.9483, 16.0135],
+        address: "Peschici, Puglia, Italy"
+    },
+    
+    // Polignano a Mare
+    {
+        name: "Al 2 Ghiottoni al Mare",
+        city: "Polignano a Mare",
+        coords: [40.9955, 17.2205],
+        address: "Polignano a Mare, Puglia, Italy"
+    },
+    {
+        name: "La Casa del Mojito",
+        city: "Polignano a Mare",
+        coords: [40.9955, 17.2205],
+        address: "Polignano a Mare, Puglia, Italy"
+    },
+    {
+        name: "Mint Cucina Fresca",
+        city: "Polignano a Mare",
+        coords: [40.9955, 17.2205],
+        address: "Polignano a Mare, Puglia, Italy"
+    },
+    {
+        name: "Pescaria",
+        city: "Polignano a Mare",
+        coords: [40.9955, 17.2205],
+        address: "Polignano a Mare, Puglia, Italy"
+    },
+    
+    // Putignano
+    {
+        name: "Angelo Sabatelli",
+        city: "Putignano",
+        coords: [40.8514, 17.1213],
+        address: "Putignano, Puglia, Italy"
+    },
+    
+    // San Vito dei Normanni
+    {
+        name: "Antiko – Spizzicaturi & Miscele",
+        city: "San Vito dei Normanni",
+        coords: [40.6562, 17.7084],
+        address: "San Vito dei Normanni, Puglia, Italy"
+    },
+    {
+        name: "Boo! Caffetteria e Bollicine",
+        city: "San Vito dei Normanni",
+        coords: [40.6562, 17.7084],
+        address: "San Vito dei Normanni, Puglia, Italy"
+    },
+    {
+        name: "Emalu Pasticceria Artigianale",
+        city: "San Vito dei Normanni",
+        coords: [40.6562, 17.7084],
+        address: "San Vito dei Normanni, Puglia, Italy"
+    },
+    {
+        name: "Enobottega Fantini",
+        city: "San Vito dei Normanni",
+        coords: [40.6562, 17.7084],
+        address: "San Vito dei Normanni, Puglia, Italy"
+    },
+    {
+        name: "Seafood Friggitoria Mastrangelo",
+        city: "San Vito dei Normanni",
+        coords: [40.6562, 17.7084],
+        address: "San Vito dei Normanni, Puglia, Italy"
+    },
+    {
+        name: "Strato Bar",
+        city: "San Vito dei Normanni",
+        coords: [40.6562, 17.7084],
+        address: "San Vito dei Normanni, Puglia, Italy"
+    },
+    {
+        name: "xfood Ristorante Sociale",
+        city: "San Vito dei Normanni",
+        coords: [40.6562, 17.7084],
+        address: "San Vito dei Normanni, Puglia, Italy"
+    },
+    
+    // Savelletri
+    {
+        name: "MòMò",
+        city: "Savelletri",
+        coords: [40.8617, 17.4721],
+        address: "Savelletri, Puglia, Italy"
+    },
+    {
+        name: "Osteria del Porto",
+        city: "Savelletri",
+        coords: [40.8617, 17.4721],
+        address: "Savelletri, Puglia, Italy"
+    },
+    {
+        name: "Pescheria 2 Mari",
+        city: "Savelletri",
+        coords: [40.8617, 17.4721],
+        address: "Savelletri, Puglia, Italy"
+    },
+    {
+        name: "Ristorante AlbaChiara",
+        city: "Savelletri",
+        coords: [40.8617, 17.4721],
+        address: "Savelletri, Puglia, Italy"
+    },
+    
+    // Taranto
+    {
+        name: "Ristorante Al Canale",
+        city: "Taranto",
+        coords: [40.4752, 17.2281],
+        address: "Taranto, Puglia, Italy"
+    },
+    {
+        name: "Fichi & Barbera Wine Bar",
+        city: "Taranto",
+        coords: [40.4752, 17.2281],
+        address: "Taranto, Puglia, Italy"
+    },
+    {
+        name: "Ristò Fratelli Pesce",
+        city: "Taranto",
+        coords: [40.4752, 17.2281],
+        address: "Taranto, Puglia, Italy"
+    },
+    {
+        name: "Gente di Mare Trattoria",
+        city: "Taranto",
+        coords: [40.4752, 17.2281],
+        address: "Taranto, Puglia, Italy"
+    },
+    {
+        name: "La ringhiera pizzeria Di Michele Ruggiero",
+        city: "Taranto",
+        coords: [40.4752, 17.2281],
+        address: "Taranto, Puglia, Italy"
+    },
+    
+    // Trani
+    {
+        name: "Gallo",
+        city: "Trani",
+        coords: [41.2804, 16.4172],
+        address: "Trani, Puglia, Italy"
+    },
+    {
+        name: "Le Lampare al Fortino",
+        city: "Trani",
+        coords: [41.2804, 16.4172],
+        address: "Trani, Puglia, Italy"
+    },
+    {
+        name: "Quintessenza Ristorante",
+        city: "Trani",
+        coords: [41.2804, 16.4172],
+        address: "Trani, Puglia, Italy"
+    },
+    
+    // Tricase
+    {
+        name: "Farmacia Balboa",
+        city: "Tricase",
+        coords: [39.9344, 18.3927],
+        address: "Tricase, Puglia, Italy"
+    },
+    {
+        name: "Locanda del Levante",
+        city: "Tricase",
+        coords: [39.9344, 18.3927],
+        address: "Tricase, Puglia, Italy"
+    }
+];
